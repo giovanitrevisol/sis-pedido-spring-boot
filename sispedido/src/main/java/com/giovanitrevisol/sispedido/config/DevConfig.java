498ac0578559f09296812afadae326c5dd9ec972
Giovani Trevisol <giovanitrevisol93@gmail.com>
2020-12-29 21:30:25 -0300

    teste

--- sispedido/src/main/java/com/giovanitrevisol/sispedido/config/DevConfig.java
+++ sispedido/src/main/java/com/giovanitrevisol/sispedido/config/DevConfig.java
@@ -2,6 +2,7 @@ package com.giovanitrevisol.sispedido.config;
 
 import com.giovanitrevisol.sispedido.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,8 +16,15 @@ public class DevConfig {
     @Autowired
     private DBService dbService;
 
+        //pegar o valor da chave que esta em application.properties
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
     @Bean
     public boolean instantiateDataBase() throws ParseException {
+        if(!"create".equals(strategy)){
+            return false;
+        }
         dbService.instantiateTestDataBase();
         return true;
     }
